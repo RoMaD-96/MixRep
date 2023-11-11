18b4b15c374bc4a840cc00e09d8e48b3d67f9968
RoMacD <ro.macridemartino@gmail.com>
2023-11-11 22:46:23 +0100

    Fixing Plots
    Renaming the file in a clear way. Adding the HPDI and Posterior with varying weights plots in the Fixed_Weight.R file

--- RepMixFun.R
+++ RepMixFun.R
@@ -1,4 +1,29 @@
 #   ____________________________________________________________________________
+#   Libraries                                                               ####
+
+library(ggplot2)
+library(ggpubr)
+library(colorspace)
+library(tipmap)
+library(spatstat)
+
+#   ____________________________________________________________________________
+#   Median Function                                                         ####
+
+median_fun <- function(theta, tr, sr, to, so, null, priorsd, w) {
+  # Ensure the data is sorted by theta
+  density <-  rmapPostFix(theta = theta, tr = tr, sr = sr, to = to, so = so,
+                          null = null, priorsd = priorsd, w = w)
+  data <- data.frame(theta, density)
+  data <- data[order(data$theta),]
+  w_median <- weighted.median(data[,1], data[,2], na.rm = TRUE)
+  
+  # Return the median value of theta
+  return(w_median)
+}
+
+
+#   ____________________________________________________________________________
 #   Normalizing Constant Function (No Integration)                          ####
 
 normConst <- function(tr, sr, to, so, null, priorsd, x, y) {
@@ -23,6 +48,21 @@ normConst <- function(tr, sr, to, so, null, priorsd, x, y) {
 #   ____________________________________________________________________________
 #   Posterior Density Functions                                             ####
 
+
+##  ............................................................................
+##  Posterior Distribution (Fixed Weights)                                  ####
+
+rmapPostFix <- function(theta, w,  tr, sr, to, so, null, priorsd, x, y) {
+  v1 <- 1/(1/so^2 + 1/sr^2)
+  m1 <- (to/so^2 + tr/sr^2)*v1
+  v2 <- 1/(1/priorsd^2 + 1/sr^2)
+  m2 <- (null/priorsd^2 + tr/sr^2)*v2
+  bf <- dnorm(x = tr, mean = to, sd = sqrt(sr^2 + so^2))/
+    dnorm(x = tr, mean = null, sd = sqrt(sr^2 + priorsd^2))
+  wUpdate <- 1/(1 + (1/w - 1)/bf)                     # Update based on the BF ?
+  wUpdate*dnorm(x = theta, mean = m1, sd = sqrt(v1)) +
+    (1 - wUpdate)*dnorm(x = theta, mean = m2, sd = sqrt(v2))
+}
 
 ##  ............................................................................
 ##  Joint Posterior Distribution                                            ####
@@ -54,12 +94,12 @@ m_post_weights <- function(w, tr, sr, to, so, null, priorsd, x, y) {
 
 m_post_theta <- function(theta, tr, sr, to, so, null, priorsd, x, y) {
   mean_beta <- x / (x + y)
-  num_post_weights <- (dnorm(x = tr, mean = theta, sd = sr) *
+  num_post_theta <- (dnorm(x = tr, mean = theta, sd = sr) *
                       (mean_beta *(dnorm(x = theta, mean = to, sd = so)
                       - dnorm(x = theta, mean = null, sd = priorsd))
                       + dnorm(x = theta, mean = null, sd = priorsd)))
-  den_post_weights <- normConst(tr, sr, to, so, null, priorsd, x, y)
-  m_post <- num_post_weights / den_post_weights
+  den_post_theta <- normConst(tr, sr, to, so, null, priorsd, x, y)
+  m_post <- num_post_theta / den_post_theta
   return(m_post)
 }
 
@@ -67,6 +107,50 @@ m_post_theta <- function(theta, tr, sr, to, so, null, priorsd, x, y) {
 #   ____________________________________________________________________________
 #   HPDI Computation                                                        ####
 
+
+
+##  ............................................................................
+##  HPDI using Fixed Weights                                                ####
+
+HPDI_post_m_theta_fix <- function(level, tr, sr, to, so, w, null, priorsd,
+                              thetaRange = tr + c(-1, 1)*qnorm(p = (1 + level)/2)*sr*3,
+                              quantileRange = c((1 - level)*0.2, (1 - level)*0.8)) {
+  # Posterior Quantile Function
+  quantile_fun <- function(q) {
+    if (q == 0) {
+      res <- -Inf
+    } else if (q == 1) {
+      res <- Inf
+    } else {
+      theta_dens_fun <- function(theta) {
+        rmapPostFix(theta = theta, tr = tr, sr = sr, to = to, so = so,
+                    null = null, priorsd = priorsd, w = w)
+      }
+      root_fun <- function(x) {
+        integrate(f = theta_dens_fun, lower = -Inf, upper = x)$value - q
+      }
+      res <- uniroot(f = root_fun, interval = thetaRange)$root
+    }
+    return(res)
+  }
+  
+  # Smallest HPDI
+  opt_fun_scalar <- function(lower_q) {
+    width <- quantile_fun(q = lower_q + level) - quantile_fun(q = lower_q)
+    return(width)
+  }
+  opt_fun_vec <- Vectorize(FUN = opt_fun_scalar)
+  opt_min_lower <- try(optim(par = (1 - level)/2, fn = opt_fun_vec,
+                             method = "L-BFGS-B", lower = quantileRange[1],
+                             upper = quantileRange[2])$par)
+  if (inherits(opt_min_lower, "try-error")) {
+    cr_int <- c("lower" = NaN, "upper" = NaN)
+  } else {
+    cr_int <- c("lower" = quantile_fun(q = opt_min_lower),
+                "upper" = quantile_fun(q = opt_min_lower + level))
+  }
+  return(cr_int)
+}
 
 ##  ............................................................................
 ##  Weights HPDI                                                            ####
